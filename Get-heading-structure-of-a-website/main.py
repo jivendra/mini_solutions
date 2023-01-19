704aa81c7b34609c222c982af52ca788a6e27d38
Jivendra <jivendrasah33@gmail.com>
2023-01-20 00:35:30 +0530

    Get Website Structure

--- Get-heading-structure-of-a-website/main.py
+++ Get-heading-structure-of-a-website/main.py
@@ -1,21 +1,29 @@
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
 import re
 from bs4 import BeautifulSoup
-html = urlopen('https://kubernetes.io/docs/concepts/services-networking/service/')
+link = "https://kubernetes.io/docs/concepts/services-networking/service/"
+
+req = Request(
+    url=link,
+    headers={'User-Agent': 'Mozilla/5.0'}
+)
+html = urlopen(req)
 bs = BeautifulSoup(html, "html.parser")
 titles = bs.find_all(['h1', 'h2','h3','h4','h5','h6'])
-
-f = open('output.txt', 'w+')
+f = open('./output.txt', 'w+')
 for title in titles:
-    content = re.search(">.+<",str(title))
-    heading = re.search("<..",str(title)).group()[1:]
-    spacing = ""
-    match heading:
-        case "h1": spacing = "\n"
-        case "h2": spacing = "\n  "
-        case "h3": spacing = "      "
-        case "h4": spacing = "           "
-        case "h5": spacing = "                "
-        case "h6": spacing = "                      "
-    f.writelines(spacing + str(content.group()[1:-1]) + "\n")
+    try:
+        content = re.search(">\\n*\\s*.+\\s*\\n*<",str(title))
+        heading = re.search("<..",str(title)).group()[1:]
+        spacing = ""
+        match heading:
+            case "h1": spacing = "\n"
+            case "h2": spacing = "\n  "
+            case "h3": spacing = "      "
+            case "h4": spacing = "           "
+            case "h5": spacing = "                "
+            case "h6": spacing = "                      "
+        f.writelines(spacing + (content.group()[1:-1]).strip() + "\n")
+    except:
+            f.write("Error: Need debugging" + "\n")
 f.close()
